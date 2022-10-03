ef69b705f4f2985f88233cdd1290b349541492ea
ERIK M. DODD <emdodd27@gmail.com>
2022-10-03 15:39:24 -0700

    updated ReadME, finished backend, added lab-clear and lab-seed js

--- server.js
+++ server.js
@@ -6,21 +6,40 @@ const express = require('express');
 const cors = require('cors');
 
 // Connect Mongoose
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 
 // Mongoose
-// mongoose.connect(process.env.DB_URL); 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'Mongo connection error'));
-// db.once('open', function() {
-  // console.log('Mongoose is connected to mongo');
-// });
+mongoose.connect(process.env.DB_URL); 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Mongo connection error'));
+db.once('open', function() {
+  console.log('Mongoose is connected to mongo');
+});
+
+const Books = require('./models/book.js');
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+
+// Endpoints
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+
+app.get('/book', getBooks);
+
+async function getBooks(req,res) {
+  try {
+    const results = await Books.find();
+    res.status(200).send(results);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
 
 app.get('/test', (request, response) => {
 
@@ -29,3 +48,4 @@ app.get('/test', (request, response) => {
 })
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
